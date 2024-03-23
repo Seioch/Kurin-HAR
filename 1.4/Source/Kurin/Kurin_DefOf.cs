d4619f286bdb2cbd9d4a7fc4f7408823b6f914f7
Seioch <lightbluespectre@gmail.com>
2024-03-23 14:43:48 -0700

    Mass update. Single Tail code

--- 1.4/Source/Kurin/Kurin_DefOf.cs
+++ 1.4/Source/Kurin/Kurin_DefOf.cs
@@ -8,30 +8,8 @@ namespace NineTailedFox
 	[DefOf]
 	public static class Kurin_DefOf
 	{
-		/*public static ThingDef_AlienRace Kurin_Race;
+        public static BodyPartDef Kurin_Tail;
 
-		public static ScenarioDef Kurin_Scenario;
-
-		public static FactionDef Kurin_Player;
-
-		public static FactionDef Kurin_KurinFaction;
-
-		public static PawnKindDef Kurin_Colonist;
-
-		public static PawnKindDef Kurin_Citizen;
-
-		public static PawnKindDef Kurin_Vigilante;
-
-		public static PawnKindDef Kurin_Trader;
-
-		public static PawnKindDef Kurin_Elite;
-
-		public static PawnKindDef Kurin_Soldier;
-
-		public static PawnKindDef Kurin_EliteSoldier;
-
-		public static PawnKindDef Kurin_Sniper;
-*/
 		public static HediffDef Kurin_NooseTrapHediff;
 	}
 }
