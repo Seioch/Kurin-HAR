982b9197e8159ac5b60b839beb7aedfd0ef25a36
Seioch <lightbluespectre@gmail.com>
2024-03-27 15:54:18 -0700

    1.5 Updates

---
+++ 1.5/Source/Kurin/Kurin_DefOf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AlienRace;
+using RimWorld;
+using Verse;
+
+namespace Kurin
+{
+	[DefOf]
+	public static class Kurin_DefOf
+	{
+        public static BodyPartDef Kurin_Tail;
+
+		public static HediffDef Kurin_NooseTrapHediff;
+	}
+}
